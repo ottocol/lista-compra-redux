48045c9bd4adb92acb432663373059603bcdb84c
Otto Colomina Pardo <otto@dccia.ua.es>
2017-11-19 20:42:07 +0100

    limpiado ejemplo inicial

--- js/modulo.js
+++
@@ -1,7 +0,0 @@
-function saludar(nombre) {
-    return "Hola qué tal, " + nombre
-}
-
-//Esto es un export con nombre, donde queramos importarlo hay que hacerlo
-//con el mismo nombre: import {saludar}
-export {saludar}
