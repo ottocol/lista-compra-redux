d8a08bfa88d54dc8461c82d49f3a5f90e01b8366
Otto Colomina Pardo <otto@dccia.ua.es>
2017-11-19 22:20:10 +0100

    cambiado a librería redux

--- js/main.js
+++ js/main.js
@@ -1,14 +1,17 @@
-import store from './redux/store'
+import { createStore } from 'redux'
 import { ADD_ITEM } from './redux/acciones';
 import rootReducer from './redux/reducers'
 
-//por ahora no usamos la librería redux, sino código propio
 //aplicamos las acciones manualmente y mostramos el resultado en la consola
-console.log(store.state)
-var a1 = { type: ADD_ITEM, nombreItem: 'pan' }
-store.state = rootReducer(store.state, a1)
-console.log(store.state)
-var a2 = { type: ADD_ITEM, nombreItem: 'patatas' }
-store.state = rootReducer(store.state, a2)
-console.log(store.state)
+var store = createStore(rootReducer)
+console.log(store.getState())
 
+store.subscribe(function(){
+    console.log(store.getState())
+})
+
+var a1 = { type: ADD_ITEM, nombreItem: 'pan' }
+store.dispatch(a1)
+
+var a2 = { type: ADD_ITEM, nombreItem: 'patatas' }
+store.dispatch(a2)
